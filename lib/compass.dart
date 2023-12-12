26ff2ce566700f50ea72bd65c2b3711346215078
keyszl <keyszl@hendrix.edu>
2023-12-12 13:02:11 -0600

    rename of project

--- lib/compass.dart
+++ lib/compass.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:sensors_plus/sensors_plus.dart';
@@ -22,6 +24,46 @@ class CompassScreen extends StatefulWidget {
     WidgetsBinding.instance
         .addPostFrameCallback((_) => _determinePosition());
   }*/
+
+void initState(){
+  initState();
+  startServer();
+}
+
+
+const hostname = '127.0.0.1'; // Binds to all adapters
+const port = 8000;
+ 
+Future<void> startServer() async {
+  final server = await ServerSocket.bind(hostname, port);
+  print('TCP server started at ${server.address}:${server.port}.');
+ 
+  try {
+    server.listen((Socket socket) {
+    print(
+        'New TCP client ${socket.address.address}:${socket.port} connected.');
+      socket.writeln("Hello from the echo server!");
+      socket.writeln("How are you?");
+      socket.listen(
+        (Uint8List data) {
+          if (data.length > 0 && data.first == 10) return;
+          final msg = data.toString();
+          print('Data from client: $msg');
+          socket.add(utf8.encode("Echo: "));
+          socket.add(data);
+        },
+        onError: (error) {
+         print('Error for client ${socket.address.address}:${socket.port}.');
+        },
+        onDone: () {
+          print('Connection to client ${socket.address.address}:${socket.port} done.');
+      });
+      });
+   } on SocketException catch (ex) {
+    print(ex.message);
+  }
+}
+
 
 class CompassScreenState extends State<CompassScreen> {
   CompassScreenState() {
